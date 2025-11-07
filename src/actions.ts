61a9fb179f5bc761db0f4ff52aaf2393f65c5ab0
JaswaniV <vjaswan78@gmail.com>
2025-11-07 22:02:15 +0530

    A74.1 uploaded

--- src/actions.ts
+++
@@ -1,6 +0,0 @@
-export const happyButtonClicked={
-    type:"happy button clicked"
-}
-export const sadButtonClicked={
-    type:"sad button clicked"
-}
--- src/selectors.ts
+++
@@ -1,8 +0,0 @@
-import type { State } from "./store";
-
-export function happyCountSelector(State:State){
-    return State.happyCount;
-}
-export function SadCountSelector(State:State){
-    return State.sadCount;
-}
